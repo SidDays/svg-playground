6a34e684470bf8f0824cdaca9690871073f178c1
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-07-12 15:01:32 -0700

    Hardcode y2 of noseline

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Eye color
 
 document.getElementById("eyesGreen").addEventListener("click", function (event) {
-  document.querySelectorAll(".eyeball").forEach(function(el) {
+  document.querySelectorAll(".eyeball").forEach(function (el) {
     el.classList.add("green-eyes");
   })
   console.log('Changed eye color to green!');
 });
 
 document.getElementById("eyesBlack").addEventListener("click", function (event) {
-  document.querySelectorAll(".eyeball").forEach(function(el) {
+  document.querySelectorAll(".eyeball").forEach(function (el) {
     el.classList.remove("green-eyes");
   })
   console.log('Changed eye color to black!');
@@ -30,12 +30,16 @@ document.getElementById("eyesBlack").addEventListener("click", function (event) 
 
 document.getElementById("sad").addEventListener("click", function (event) {
   document.querySelector("#mouthSadLine").classList.add('mouth-sad');
-  document.querySelector("#noseSadLine").classList.add('nose-sad');
-  console.log('Changed expression to sad!');
-});
-
-document.getElementById("happy").addEventListener("click", function (event) {
-  document.querySelector("#mouthSadLine").classList.remove('mouth-sad');
-  document.querySelector("#noseSadLine").classList.remove('nose-sad');
+  document.querySelector("#noseSadLine")
+    .setAttribute("y2", 289.817524);
+    // .classList.add('nose-sad');
+    console.log('Changed expression to sad!');
+  });
+  
+  document.getElementById("happy").addEventListener("click", function (event) {
+    document.querySelector("#mouthSadLine").classList.remove('mouth-sad');
+    document.querySelector("#noseSadLine")
+    // .classList.remove('nose-sad');
+    .setAttribute("y2", 340.817524);
   console.log('Changed expression to happy!');
 });
