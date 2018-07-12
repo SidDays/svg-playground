208f3cf9e981de3422f2237e086c3ff5d990b05e
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-07-12 14:24:54 -0700

    Modify expression on click, Japanese placeholder

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Eye color
+
 document.getElementById("eyesGreen").addEventListener("click", function (event) {
   document.querySelectorAll(".eyeball").forEach(function(el) {
     el.classList.add("green-eyes");
@@ -10,4 +12,18 @@ document.getElementById("eyesBlack").addEventListener("click", function (event) 
     el.classList.remove("green-eyes");
   })
   console.log('Changed eye color to black!');
+});
+
+// Expression
+
+document.getElementById("sad").addEventListener("click", function (event) {
+  document.querySelector("#mouthSad").setAttribute("display", "inline");
+  document.querySelector("#mouthHappy").setAttribute("display", "none");
+  console.log('Changed expression to sad!');
+});
+
+document.getElementById("happy").addEventListener("click", function (event) {
+  document.querySelector("#mouthSad").setAttribute("display", "none");
+  document.querySelector("#mouthHappy").setAttribute("display", "inline");
+  console.log('Changed expression to happy!');
 });
