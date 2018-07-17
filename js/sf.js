d369bb7a5884d83a551699e55753d85a0f28317b
Aayush Khanna <aayushkh@usc.edu>
2018-07-17 10:05:15 -0700

    skrink-in ! :100:

--- js/sf.js
+++ js/sf.js
@@ -1,7 +1,7 @@
 $(function() {
     /* 
-        rows - letters a,b,c,d
-        columns - numbers 1,2,3,4
+    rows - letters a,b,c,d
+    columns - numbers 1,2,3,4
     */ 
     var a1 = $('.a1'),
         a2 = $('.a2'),
@@ -20,20 +20,51 @@ $(function() {
         d3 = $('.d3'),
         d4 = $('.d4');
 
-    // innitialize 
-    document.querySelector("#frame").remove();
+    /*
+    reuseable variables
+    */
+    var row;
+    var blockName;
+
+    /*
+    innitial positions of the blocks
+    */
+    for (var i=1; i<=4 ; i++) {
+        var x, y , x_mod, y_mod;
+        row = String.fromCharCode(96 + i);
+        for (var col=1; col<=4 ; col++) {
+            blockName = row + col;                        
+            x_mod = (col<=2) ? -100 : 100;
+            y_mod = (i<=2) ? -100 : 100;
+            if($('.' + blockName).hasClass('diagonal')) {
+                x = x_mod;
+                y = y_mod;
+            } else {
+                if (i==1) {
+                    x = x_mod * 0.5;
+                    y = y_mod;
+                } else if (i==2) {
+                    x = x_mod;
+                    y = y_mod * 0.5;
+                } else if (i==3) {
+                    x = x_mod;
+                    y = y_mod * 0.5;
+                } else if (i==4) {
+                    x = x_mod * 0.5;
+                    y = y_mod;
+                }
+            }
+            $('.' + blockName).css('transform', 'translate(' + x + '%,' + y + '%' + ')');
+        }
+    }
 
     /* 
         slide-in {that's what she said}
     */
-
-    var row;
-    var blockName;
     for (var i=1; i<=4 ; i++) {
         row = String.fromCharCode(96 + i);
         for (var col=1; col<=4 ; col++) {
             blockName = row + col;
-            console.log(blockName);
 
             if (col<=2) {
                 $('.' + blockName).addClass('slide-in-left');
